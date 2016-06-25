f7f3d6cfe511ddad20c4ea6f457cb8657a8f6e50
Nicholas Palermo <npalermo10@gmail.com>
2016-06-25 13:28:59 -0400

    fixed some serious style guidelines

--- pdb_emacs.el
+++ pdb_emacs.el
@@ -5,61 +5,50 @@
 ;;; Code:
 
 (defun my-insert-pdb-break ()
-  "insert a pdb.set_trace() at point"
+  "insert a pdb.set_trace() at point."
   (interactive)
   (beginning-of-line)
   (newline)
-  (previous-line)
-  (insert-string "pdb.set_trace()")
-  (py-indent-line-outmost)
-  )
+  (forward-line -1)
+  (insert "pdb.set_trace()")
+  (py-indent-line-outmost))
 
 (defun my-insert-import-pdb()
   "insert import pdb at top if not already there"
   (interactive)
-  (goto-char(point-max))
-  (if (search-backward "import pdb" nil t)
-      ()
-    (if (re-search-backward "#!.*python.*" nil t)
+  (goto-char(point-min))
+  (unless (search-forward "import pdb" nil t)
+    (if (re-search-forward "#!.*python.*" nil t)
         (progn
-         (end-of-line)
-         (newline)
-         (insert-string "import pdb")
-         (py-indent-line-outmost)
-         )
-
+          (end-of-line)
+          (newline))
       (goto-char(point-min))
       (newline)
-      (previous-line)
-      (insert-string "import pdb")
-      (py-indent-line-outmost))
-    )
-
-  )
+      (forward-line -1))
+    (insert "import pdb")
+    (py-indent-line-outmost)))
 
 (defun my-insert-pdb-import-and-break()
   "imports pdb (if needed) and sets pdb break"
   (interactive)
   (save-excursion
     (my-insert-pdb-break)
-    (my-insert-import-pdb)
-    )
-  )
+    (my-insert-import-pdb)))
 
 (defun my-pdb-cleanup()
   "deletes any import pdb and pdb.set_trace()"
   (interactive)
   (save-excursion
-    (goto-char(point-max))
-    (while
-        (search-backward "pdb.set_trace()" nil t)
-      (beginning-of-line)
-      (kill-line 1))
-    (search-backward "import pdb" nil t)
+    (goto-char(point-min))
+    (search-forward "import pdb" nil t)
     (beginning-of-line)
     (kill-line 1)
-    )
-  )
+    (goto-char(point-min))
+    (while
+        (search-forward "pdb.set_trace()" nil t)
+      (beginning-of-line)
+      (kill-line 1))
+    ))
 
 
 (provide 'pdb_emacs)
