c39782a1ddc9ffa2f9eacfd0f7ccabbde9cfecd5
Nicholas Palermo <npalermo10@gmail.com>
2016-06-26 19:37:26 -0400

    pdb now works even on narrowed buffer

--- pdb_emacs.el
+++ pdb_emacs.el
@@ -1,13 +1,13 @@
 ;;; pdb_emacs.el --- Insert and remove pdb import and traces
-; Package-Requires: ((python-mode))
+                                        ; Package-Requires: ((python-mode))
 ;;; Commentary:
 
-;;This package eases the use of pdb for python debugging by providing functions to quickly insert pdb.set_trace() and import pdb into your code. Also provides a pdb cleanup function. 
+;;This package eases the use of pdb for python debugging by providing functions to quickly insert pdb.set_trace() and import pdb into your code.  Also provides a pdb cleanup function.
 
 ;;; Code:
 
 (defun my-insert-pdb-break ()
-  "insert a pdb.set_trace() at point."
+  "Insert a pdb.set_trace() at point."
   (interactive)
   (beginning-of-line)
   (newline)
@@ -18,17 +18,19 @@
 (defun my-insert-import-pdb()
   "insert import pdb at top if not already there"
   (interactive)
-  (goto-char(point-min))
-  (unless (search-forward "import pdb" nil t)
-    (if (re-search-forward "#!.*python.*" nil t)
-        (progn
-          (end-of-line)
-          (newline))
-      (goto-char(point-min))
-      (newline)
-      (forward-line -1))
-    (insert "import pdb")
-    (py-indent-line-outmost)))
+  (save-restriction
+    (widen)
+    (goto-char(point-min))
+    (unless (search-forward "import pdb" nil t)
+      (if (re-search-forward "#!.*python.*" nil t)
+          (progn
+            (end-of-line)
+            (newline))
+        (goto-char(point-min))
+        (newline)
+        (forward-line -1))
+      (insert "import pdb")
+      (py-indent-line-outmost))))
 
 (defun my-insert-pdb-import-and-break()
   "imports pdb (if needed) and sets pdb break"
@@ -40,17 +42,19 @@
 (defun my-pdb-cleanup()
   "deletes any import pdb and pdb.set_trace()"
   (interactive)
-  (save-excursion
-    (goto-char(point-min))
-    (search-forward "import pdb" nil t)
-    (beginning-of-line)
-    (kill-line 1)
-    (goto-char(point-min))
-    (while
-        (search-forward "pdb.set_trace()" nil t)
+  (save-restriction
+    (widen)
+    (save-excursion
+      (goto-char(point-min))
+      (search-forward "import pdb" nil t)
       (beginning-of-line)
-      (kill-line 1))
-    ))
+      (kill-line 1)
+      (goto-char(point-min))
+      (while
+          (search-forward "pdb.set_trace()" nil t)
+        (beginning-of-line)
+        (kill-line 1))
+      )))
 
 (provide 'pdb_emacs)
 ;;; pdb_emacs.el ends here
